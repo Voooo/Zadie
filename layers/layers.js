e631becf0466680d6bf6a2ea84288ea4d547c921
Voooo <bollan_z@hotmail.com>
2026-02-04 19:57:28 +0100

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,38 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_Webmap_1 = new ol.format.GeoJSON();
+var features_Webmap_1 = format_Webmap_1.readFeatures(json_Webmap_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Webmap_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Webmap_1.addFeatures(features_Webmap_1);
+var lyr_Webmap_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Webmap_1, 
+                style: style_Webmap_1,
+                popuplayertitle: 'Webmap',
+                interactive: true,
+                title: '<img src="styles/legend/Webmap_1.png" /> Webmap'
+            });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_Webmap_1.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_Webmap_1];
+lyr_Webmap_1.set('fieldAliases', {'fid': 'fid', 'Stadsdelsområde': 'Stadsdelsområde', 'Stadsdelsnamn': 'Stadsdelsnamn', 'layer': 'layer', 'path': 'path', });
+lyr_Webmap_1.set('fieldImages', {'fid': 'Range', 'Stadsdelsområde': 'TextEdit', 'Stadsdelsnamn': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
+lyr_Webmap_1.set('fieldLabels', {'fid': 'no label', 'Stadsdelsområde': 'no label', 'Stadsdelsnamn': 'no label', 'layer': 'no label', 'path': 'no label', });
+lyr_Webmap_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
