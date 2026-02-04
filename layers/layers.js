ed8fa3b796429fcd591a61613082409a93624866
Voooo <bollan_z@hotmail.com>
2026-02-04 20:10:50 +0100

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,3 +1,5 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:3006").setExtent([654282.210213, 6571757.225258, 687530.202039, 6592395.386314]);
 var wms_layers = [];
 
 
@@ -14,7 +16,7 @@ var wms_layers = [];
         });
 var format_Webmap_1 = new ol.format.GeoJSON();
 var features_Webmap_1 = format_Webmap_1.readFeatures(json_Webmap_1, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3006'});
 var jsonSource_Webmap_1 = new ol.source.Vector({
     attributions: ' ',
 });
@@ -32,7 +34,7 @@ lyr_OpenStreetMap_0.setVisible(true);lyr_Webmap_1.setVisible(true);
 var layersList = [lyr_OpenStreetMap_0,lyr_Webmap_1];
 lyr_Webmap_1.set('fieldAliases', {'fid': 'fid', 'Stadsdelsområde': 'Stadsdelsområde', 'Stadsdelsnamn': 'Stadsdelsnamn', 'layer': 'layer', 'path': 'path', });
 lyr_Webmap_1.set('fieldImages', {'fid': 'Range', 'Stadsdelsområde': 'TextEdit', 'Stadsdelsnamn': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
-lyr_Webmap_1.set('fieldLabels', {'fid': 'no label', 'Stadsdelsområde': 'no label', 'Stadsdelsnamn': 'no label', 'layer': 'no label', 'path': 'no label', });
+lyr_Webmap_1.set('fieldLabels', {'fid': 'no label', 'Stadsdelsområde': 'no label', 'Stadsdelsnamn': 'inline label - always visible', 'layer': 'no label', 'path': 'no label', });
 lyr_Webmap_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
